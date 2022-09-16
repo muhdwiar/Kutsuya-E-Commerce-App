295262492289926d7c910c06a7ab81e2c2c0583d
muhdwiar <muhdwi45@gmail.com>
2022-09-16 08:58:13 +0700

    modify add-new-cart feature

--- features/shopping_cart/usecase/logic.go
+++ features/shopping_cart/usecase/logic.go
@@ -19,6 +19,9 @@ func New(dataCart cart.DataInterface, dataProduk produk.DataInterface) cart.Usec
 }
 
 func (usecase *cartUsecase) InsertCart(dataCart cart.Core) (int, error) {
+
+	var newCart cart.Core
+
 	if dataCart.Product_Id == 0 || dataCart.User_Id == 0 {
 		return -1, errors.New("data Product_Id dan User_Id kosong")
 	}
@@ -28,13 +31,15 @@ func (usecase *cartUsecase) InsertCart(dataCart cart.Core) (int, error) {
 		return -1, errProduk
 	}
 
-	dataCart.Nama_Produk = dataProduk.Nama_Produk
-	dataCart.Ukuran = dataProduk.Ukuran
-	dataCart.Merk = dataProduk.Merk
-	dataCart.Harga = dataProduk.Harga
-	dataCart.File_Image = dataProduk.File_Image
+	newCart.User_Id = dataCart.User_Id
+	newCart.Product_Id = dataCart.Product_Id
+	newCart.Nama_Produk = dataProduk.Nama_Produk
+	newCart.Ukuran = dataProduk.Ukuran
+	newCart.Merk = dataProduk.Merk
+	newCart.Harga = dataProduk.Harga
+	newCart.File_Image = dataProduk.File_Image
 
-	row, err := usecase.cartData.CreateCart(dataCart)
+	row, err := usecase.cartData.CreateCart(newCart)
 	return row, err
 
 }
